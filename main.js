ef45d7cc023f0a4a8f053fd0f192dc19f18555f1
Jan Lucas <janlucas13@gmail.com>
2023-08-19 21:44:51 -0300

    commit adicionando main js e alterações

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.botao');
+
+form.addEventListener('submit', function (e) {
+    let formValido = false;
+    e.preventDefault();
+    
+    let campoA = document.getElementById('campoa');
+    let campoB = document.getElementById('campob');
+    let validacao = document.getElementById('validacao');
+
+    formValido = (campoB.value > campoA.value);
+    if (formValido) {
+        validacao.textContent = "Sucesso";
+        validacao.style.color = "green";
+
+        document.getElementById('validacao').innerHTML = 'Sucesso';
+        return true;
+    } else {
+        validacao.textContent = "O número do campo B deve ser maior que o número do campo A.";
+        validacao.style.color = "red";
+        return false;
+    }
+})
