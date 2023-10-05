53583e8609033c04b78b27fd22de59fe4564541e
Jan Lucas <janlucas13@gmail.com>
2023-10-04 21:37:17 -0300

    adicionando compilaSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp'); //função usada para poder usar plugins do gulp
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./build/styles'))
+}
 
 function comprimeImage() {
     return gulp.src('./source/image/*') // pasta origem
@@ -17,4 +24,5 @@ function comprimeJavaScript() {
 exports.default = function() {
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
     gulp.watch('./source/image/*', { ignoreInitial: false }, gulp.series(comprimeImage));
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
 }
