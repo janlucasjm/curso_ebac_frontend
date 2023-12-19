11ffebac3b852ad80433c5e5bb3b473ed7beb8a5
Jan Lucas <janlucas13@gmail.com>
2023-12-18 21:40:19 -0300

    array de objetos

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
-function Pessoa(nome, idade) {
-    this.nome = nome;
-    this.idade = idade;
+class NotaAluno {
+    constructor(nomeAluno, nota) {
+        this.nome = nomeAluno;
+        this.nota = nota;
+    }
+
+    media() {
+        if (this.nota >= 6) {
+            return console.log(`Aluno: ${this.nome}; Nota: ${this.nota}.`);
+        }
+    }
 }
 
-function Profissao(nome, cargo, especializacao) {
-    this.cargo = cargo;
-    this.especializacao = especializacao;
-
-    Pessoa.call(this, nome);
-}
-
-function Lazer(atividade, hobbies) {
-    this.atividade = atividade;
-    this.hobbies = hobbies;
-
-    Pessoa.call(this);
-}
-
-const funcionario1 = new Profissao('Jaqueline', 'Medica', 'Ortopedista');
-const lazer1 = new Lazer('Correr', 'Ler livros');
-
-const funcionario2 = new Profissao('Luan', 'Engenheiro', 'Metalurgia');
-const lazer2 = new Lazer('Jogar bola', 'estudar');
-
-console.log(`Olá, me chamo ${funcionario1.nome}, atualmente sou ${funcionario1.cargo}, especializado em ${funcionario1.especializacao}.`);
-console.log(`Gosto bastante de ${lazer1.atividade} e também amo ${lazer1.hobbies}!`);
-
-
-console.log(`Olá, me chamo ${funcionario2.nome}, atualmente sou ${funcionario2.cargo}, especializado em ${funcionario2.especializacao}.`);
-console.log(`Gosto bastante de ${lazer2.atividade} e também amo ${lazer2.hobbies}!`);
-
+const aluno1 = new NotaAluno('Jan', 8);
+const aluno2 = new NotaAluno('Gabriel', 4);
+aluno1.media();
+aluno2.media();
