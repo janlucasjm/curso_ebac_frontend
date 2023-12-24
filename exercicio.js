12239d2aadfb778e94317a3571421077ac98d4a0
Jan Lucas <janlucas13@gmail.com>
2023-12-24 19:08:43 -0300

    terminando exercicio exigido

---
+++ exercicio.js
@@ -0,0 +1,4 @@
+var multiplicacao = function (a, b) { return a * b; };
+console.log(multiplicacao(5, 2));
+var saudacao = function (nome) { return "Ol\u00E1 ".concat(nome); };
+console.log(saudacao('Jan'));
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (a: number, b: number): number => a * b
+
+console.log(multiplicacao(5,2))
+
+const saudacao = (nome: string): string => `Olá ${nome}`;
+
+console.log(saudacao('Jan'))
