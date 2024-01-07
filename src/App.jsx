4842c468814e485ebe938d0ac830687f946adff0
Jan Lucas <janlucas13@gmail.com>
2024-01-07 12:48:09 -0300

    melhorando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from './App.module.css'
 
 function App() {
   const [altura, setAltura] = useState(0);
   const [peso, setPeso] = useState(0);
   const [resultadoIMC, setResultadoIMC] = useState(false);
+  const imc = Number(peso / (altura * altura));
+
+  const tabelaRef = useRef(null);
 
   const renderizaImc = () => {
-    const imc = Number(peso / (altura * altura));
     return imc.toFixed(2);
   }  
 
+  const obterDadosDaTabela = () => {
+    if (tabelaRef.current) {
+      const linhas = tabelaRef.current.getElementsByTagName('tr');
+
+      for(let i = 0; i < linhas.length; i++) {
+        const idDaLinha = linhas[i].id;
+        console.log(`ID da linha ${i + 1}: ${idDaLinha}`);
+
+        if(imc >= 30 && imc <=35) {
+          return true;
+        }
+      }
+    }
+  }
+
+  function renderizar() {
+    setResultadoIMC(!resultadoIMC);
+    obterDadosDaTabela()
+  }
+
+
+  //
   return (
     <>
     <div className={styles.container}>
@@ -28,7 +52,7 @@ function App() {
       </div>
 
       <div className={styles.result}>
-        <button className={styles.button} type="button" onClick={() => setResultadoIMC(!resultadoIMC)}>Calcular</button>
+        <button className={styles.button} type="button" onClick={() => renderizar()}>Calcular</button>
 
         <p>Seu imc é: {resultadoIMC && (
           <>  
@@ -39,7 +63,7 @@ function App() {
       </div> 
 
       <div className={styles.tableContainer}>
-            <table>
+            <table ref={tabelaRef}>
               <thead>
                 <tr>
                   <th>IMC(kg/m2)</th>
@@ -47,37 +71,37 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                <tr id='table-1'>
                   <td>Menor que 16,9</td>
                   <td>Muito abaixo do peso</td>
                 </tr>
     
-                <tr>
+                <tr id='table-2'>
                   <td>17 a 18,4</td>
                   <td>Abaixo do peso</td>
                 </tr>
     
-                <tr>
+                <tr id='table-3'>
                   <td>18,5 a 24,9</td>
                   <td>Peso normal</td>
                 </tr>
     
-                <tr>
+                <tr className={obterDadosDaTabela() === true ? styles.active : ''} id='table-4'>
                   <td>25 a 29,9</td>
                   <td>Acima do peso</td>
                 </tr>
     
-                <tr>
+                <tr className={obterDadosDaTabela() === true ? styles.active : ''} id='table-5'>
                   <td>30 a 34,9</td>
                   <td>Obesidade grau 1</td>
                 </tr>
     
-                <tr>
+                <tr id='table-6'>
                   <td>35 a 40</td>
                   <td>Obesidade grau 2</td>
                 </tr>
     
-                <tr>
+                <tr id='table-7'> 
                   <td>Maior que 40</td>
                   <td>Obesidade grau 3</td>
                 </tr>
